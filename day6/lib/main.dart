58b7d2256049f1d63e7585edee3168e8c6354f20
Kamalesh <makeshkamaleshh@gmail.com>
2025-11-03 19:38:38 +0530

    Day 6 commit

---
+++ day6/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'screens/trip_detail_screen.dart';  // Import the trip detail screen
+import 'screens/destination_screen.dart';
+import 'screens/vacation_detail_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Waypath Trip Details',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+      ),
+       // home: const TripDetailScreen(),  // Changed to TripDetailScreen
+      // home:const DestinationScreen(),
+      home: const VacationDetailScreen(),
+
+    );
+  }
+}
